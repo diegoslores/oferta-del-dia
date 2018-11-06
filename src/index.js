be20c9cfe794ec4ea6ab5d7ec22f545024d9b46f
Diegos Lores <diegoslores@gmail.com>
2018-11-06 14:57:30 +0100

    Extracción de componentes.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Dave extends React.Component {
-  render() {
-    return <p>"What do you think you are doing, Dave?"</p>;
-  }
-}
-
-const Yo = () => {
-  return <p>"What do you think you are doing, Yo?"</p>;
-};
-
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Dave />
-        <Yo />
-      </>
-    );
-  }
-}
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#app-container"));
