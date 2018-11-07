8f50c4535e90fa34c34c1d2afffdebd3501facff
Diegos Lores <diegoslores@gmail.com>
2018-11-07 14:58:18 +0100

    Commit de Header, Inventory y Order

---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Inventory extends React.Component {
+  render() {
+    return <div>Todo lo inventado</div>;
+  }
+}
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Order extends React.Component {
+  render() {
+    return <div>Ordename lo que yo quiera</div>;
+  }
+}
+
+export default Order;
