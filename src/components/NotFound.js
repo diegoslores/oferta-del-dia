0af2561e5b047143cf492c7760307b8ce0377908
Diegos Lores <diegoslores@gmail.com>
2018-11-09 13:22:33 +0100

    notFound componnent changed.

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+/*const NotFound = () => <h1>Ups! 404! Page not found!</h1>;*/
+
+class NotFound extends React.Component {
+  render() {
+    return <h1>Ups! 404! Page not found!</h1>;
+  }
+}
+
+export default NotFound;
--- src/components/notFound.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const NotFound = () => <h1>Ups! 404! Page not found!</h1>;
-
-export default NotFound;
