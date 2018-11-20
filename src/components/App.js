603ef6e61157e26ed876a7c08256e2d219919963
Diegos Lores <diegoslores@gmail.com>
2018-11-20 12:00:45 +0100

    Fish y App
    Creación de Fish.js e inserción en App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import samplefishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
-    fishes: {},
-    order: {}
+    fishes: {}
   };
 
   addFish = newFish => {
@@ -18,14 +19,26 @@ class App extends React.Component {
     this.setState({ fishes: newFishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: samplefishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(fish => (
+              <Fish key={fish} fishDetails={this.state.fishes[fish]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
