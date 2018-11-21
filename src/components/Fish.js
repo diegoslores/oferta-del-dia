88e0fec8e986ed79776fdcd5ea3a053b7113a29b
Diegos Lores <diegoslores@gmail.com>
2018-11-21 13:25:46 +0100

    Fix add to order/sold out

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,6 +4,7 @@ import { formatPrice } from "../helpers";
 class Fish extends React.Component {
   render() {
     const { name, desc, image, price, status } = this.props.fishDetails;
+    const isNotAvailable = status === "unavailable";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,7 +13,9 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        <button disabled={isNotAvailable}>
+          {isNotAvailable ? "Sold Out!" : "Add to Cart"}
+        </button>
       </li>
     );
   }
