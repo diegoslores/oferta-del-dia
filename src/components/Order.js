1a01d750398c71e70a9d0183e50e5ffee5f3b008
Diegos Lores <diegoslores@gmail.com>
2018-11-23 10:28:06 +0100

    Codigo de cesta y formato completado.

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,18 +2,40 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
-  suma = (acc, value) => {
-    return this.props.fishes[value].price * this.props.order[value] + acc;
+  TotalReducer = (subTotal, value) => {
+    const fish = this.props.fishes[value];
+    const quantity = this.props.order[value];
+    const isAvailable = fish.status === "available";
+    if (isAvailable) {
+      return fish.price * quantity + subTotal;
+    } else {
+      return subTotal;
+    }
+  };
+
+  renderOrder = orderKey => {
+    const quantity = this.props.order[orderKey];
+    const { price, name, status } = this.props.fishes[orderKey];
+    const isAvailable = status === "available";
+    if (isAvailable) {
+      return (
+        <li key={orderKey}>
+          {quantity} lbs {name} {formatPrice(price * quantity)}
+        </li>
+      );
+    } else {
+      return <li key={orderKey}>Sorry {name} is not available</li>;
+    }
   };
 
   render() {
     const orderIds = Object.keys(this.props.order);
-    const total = orderIds.reduce(this.suma, 0);
+    const total = orderIds.reduce(this.TotalReducer, 0);
 
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        {orderIds}
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>
